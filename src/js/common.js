7757e4aaabc18bbd22770cafdb54f74f6f456276
Alex <amendment000@gmail.com>
2017-08-02 22:36:00 +0300

    go

---
+++ src/js/common.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+	 $("#menu").mmenu({
+	 		 "extensions": [
+                  "fx-panels-zoom"
+               ],
+               "offCanvas": {
+                  "position": "right"
+               }
+            });
+
+})
