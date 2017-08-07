cce5c2ab4f9cf759509f2880bbb3770226709bdd
Alex <amendment000@gmail.com>
2017-08-07 15:03:18 +0300

    go

--- src/js/common.js
+++ src/js/common.js
@@ -1,13 +1,31 @@
 $(document).ready(function(){
 
-	 $("#menu").mmenu({
-	 		
-	 		 "extensions": [
-                  "fx-panels-zoom"
-               ],
-               "offCanvas": {
-                  "position": "right"
-               }
-            });
+	$("#menu").mmenu({
+
+		"extensions": [
+		"fx-panels-zoom"
+		],
+		"offCanvas": {
+			"position": "right"
+		}
+
+		
+	});
+
+	 var api = $("#menu").data( "mmenu" );
+	 api.bind("open:finish", function($panel) {
+	 	$('#nav-toggle').addClass('active')
+	 });
+	 api.bind("close:finish", function($panel) {
+	 	$('#nav-toggle').removeClass('active')
+	 });
+
+	
+
+
+	/*document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function() {
+    this.classList.toggle( "active" );
+  });*/
 
 })
