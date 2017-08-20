521a0f5e51ee9a932e733885c9c25c1a98274815
Alex <amendment000@gmail.com>
2017-08-20 21:28:54 +0300

    commit

--- src/js/common.js
+++ src/js/common.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
 	$("#menu").on("click","a", function (event) {
 		event.preventDefault();
 		var id  = $(this).attr('href'),
-		top = $(id).offset().top;
+				top = $(id).offset().top - $('.top-fix').height() - 20;
 		$('body,html').animate({scrollTop: top}, 1500);
 	});	
 
